6041099c86ebe739b3a1e132ae3efc585a41a377
Zhanar0 <115440908+Zhanar0@users.noreply.github.com>
2023-12-13 17:47:15 +0600

    Update lab7-6.py

--- lab7-6.py
+++ lab7-6.py
@@ -5,14 +5,20 @@ def all_and_none(text1.txt):
 
 
 try:
+ # Открытие файла для чтения ('r'),'with open'- контекстный менеджер,котрый обеспечивает выполнение операций "входа" и "выхода" тем самым упрощает управление работы с файлами 
     with open(text1.txt, 'r') as file:
+         # Чтение всего содержимого файла и сохранение его в переменной content
         content = file.read()
+        # [content.split() ]разделяет содержимое на слова.
         words = content.split()  # Split the content into words
+         # Используем метод count() для подсчета вхождений слов 'all' и 'none'.
         count_all = words.count("all")
         count_none = words.count("none")
+        # Вывод результатов подсчета на экран
         print(f"Count of 'all': {count_all}")
         print(f"Count of 'none': {count_none}")
 except FileNotFoundError:
+     # Обработка исключения, если файл не найден
     print(f"File {file_name} not found.")
 
 # вызываем функцию
